d670cac1b6241e78c8c3cc6980ed011c31d6f32b
Omar Arguelles Arreola <omar00231@gmail.com>
2016-03-11 21:53:23 -0600

    Sum: Gregory-Leibniz sum automatic sample

---
+++ js/sum.js
@@ -0,0 +1,14 @@
+
+function sum(argument) {
+  var k = 1;
+  var limit = 1000000;
+  var result = 0;
+
+  while(k <= limit) {
+    result += Math.pow(-1, k+1) / (2*k-1);
+    k++;
+  }
+
+  document.getElementById('sum-result').value = (4 * result);
+}
+
